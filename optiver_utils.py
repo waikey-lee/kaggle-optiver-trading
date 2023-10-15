52dd5054ced0e7dd58b32ef4ff27ef60f38226bf
waikey-lee <leewaikey.ai@gmail.com>
2023-10-15 18:38:14 +0800

    Added filter_df function to allow EDA faster

--- optiver_utils.py
+++ optiver_utils.py
@@ -3,7 +3,60 @@ import pandas as pd
 from tqdm import tqdm
 
 # ========================================================================================
-# 1. Preprocessing Function for this competition
+# 1. EDA Function for this competition
+# ========================================================================================
+def filter_df(df, stock_id=None, date_id=None, seconds=None, reset_index=False):
+    """
+    Filter a DataFrame based on specified conditions.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame to be filtered.
+        stock_id (int, optional): Filter rows where the 'stock_id' column matches this value.
+        date_id (int or str, optional): Filter rows where the 'date_id' column matches this value.
+        seconds (int, tuple, or list, optional): Filter rows based on 'seconds' column conditions.
+        reset_index (bool, optional): Whether to reset the index of the filtered DataFrame.
+
+    Returns:
+        pandas.DataFrame: The filtered DataFrame based on the specified conditions.
+
+    The function filters the input DataFrame 'df' based on the specified conditions. It allows you to filter rows
+    based on values in the 'stock_id' and 'date_id' columns and apply various conditions to the 'seconds' column.
+
+    - 'stock_id': If specified, only rows with a matching 'stock_id' are retained.
+    - 'date_id': If specified, only rows with a matching 'date_id' are retained. 'date_id' can be provided as an int or str.
+    - 'seconds': If specified as an int, only rows with 'seconds' matching the value are retained.
+                If specified as a tuple, only rows with 'seconds' within the specified range are retained.
+                If specified as a list, only rows with 'seconds' matching values in the list are retained.
+
+    The filtered DataFrame is returned, and you can choose to reset its index using the 'reset_index' parameter.
+    """
+    # For each argument, get the conditons series based on the input data format
+    conds_dict = {}
+    for input_, input_arg_name in zip([stock_id, date_id, seconds], ["stock_id", "date_id", "seconds"]):
+        if input_ is None:
+            conds_dict[input_arg_name] = ~df[input_arg_name].isnull()
+        elif isinstance(input_, int):
+            conds_dict[input_arg_name] = (df[input_arg_name] == input_)
+        elif isinstance(input_, str):
+            conds_dict[input_arg_name] = (df[input_arg_name] == int(input_))
+        elif isinstance(input_, tuple):
+            if len(stock_id) == 2:
+                conds_dict[input_arg_name] = df[input_arg_name].between(input_[0], input_[1])
+            else:
+                print(f"{input_arg_name} tuple shouldn't have more than 2 dimension")
+        elif isinstance(input_, list):
+            conds_dict[input_arg_name] = df[input_arg_name].isin(input_)
+    
+    # Filter the dataframe using AND (&) Operator
+    conditions = [conds for conds in conds_dict.values()]
+    df_subset = df.loc[pd.concat(conditions, axis=1).all(axis=1)]
+    
+    if reset_index:
+        df_subset = df_subset.reset_index(drop=True)
+    return df_subset
+
+# ========================================================================================
+# 2. Preprocessing Function for this competition
 # ========================================================================================
 def clean_df(df):
     df = df.drop(columns=['row_id', 'time_id'], errors="ignore")
@@ -18,9 +71,8 @@ def clean_df(df):
     )
     return df
 
-
 # ========================================================================================
-# Postprocessing Function for this competition
+# 3. Postprocessing Function for this competition
 # ========================================================================================
 def goto_conversion(listOfOdds, total=1, eps=1e-6, isAmericanOdds=False):
 
